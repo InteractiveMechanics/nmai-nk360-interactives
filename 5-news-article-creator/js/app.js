e32587885326d763baf50d492063196734a8770d
Michael Tedeschi <mike@interactivemechanics.com>
2017-08-11 16:15:07 -0400

    Update app.js

--- 5-news-article-creator/js/app.js
+++ 5-news-article-creator/js/app.js
@@ -1,3 +1,8 @@
+/**
+ * Loads data and initializes all functions
+ * Data is scoped as a global variable
+ */
+
 var data = function() {};
 $(function(){
     $.get('./data/m4a1.json', function(response) {
