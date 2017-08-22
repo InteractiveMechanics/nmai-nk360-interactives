cc415c4e7f42b75faf3290d9f563716dad1d6cc4
Michael Tedeschi <mike.tedeschi@gmail.com>
2017-08-22 10:04:24 -0400

    Add basic project structure for all interactives

---
+++ 1-salmon-challenges/js/app.js
@@ -0,0 +1,15 @@
+/**
+ * Loads data and initializes all functions
+ * Data is scoped as a global variable
+ */
+
+var data = function() {};
+$(function(){
+    $.get('./data/m3a1.json', function(response) {
+        data = response;
+
+        Init.init();
+        Instructions.init();
+
+    }, 'json');
+});
---
+++ 2-timeline/js/init.js
@@ -0,0 +1,12 @@
+/**
+ * All setup and init functions, data and event binding
+ */
+Init = (function() {
+    var init = function() {
+
+    }
+    
+    return {
+        init: init
+    }
+})();
