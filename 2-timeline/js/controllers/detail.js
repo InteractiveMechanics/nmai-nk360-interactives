0f9b990d1240e52bd03c2257a8d45bfc9eef66d8
Christina Deemer <cahdeemer@gmail.com>
2017-08-25 17:22:27 -0400

    dragdrop functionality

--- 2-timeline/js/controllers/detail.js
+++ 2-timeline/js/controllers/detail.js
@@ -9,11 +9,59 @@ Detail = (function() {
     	//$('#detail-template').tmpl().appendTo('#detail');
     	$('#detail').removeClass('hidden').addClass('animated fadeIn');
         Init.isSelectionScreen();
+        buildGame();
     }
 
 
+    var buildGame = function() {
+        $('.draggable-widget').draggable({
+            snap: '.droppable-widget',
+            //revert: 'invalid',
+            snapMode: 'interior',
+            snapTolerance: 20,
+            cursorAt: {top: 10, left: 100}
+        });
+
+        $('.droppable-widget').droppable({
+            drop: function( event, ui ) {
+                var droppableNumber = $(this).data('timeline');
+                var originalLeft = $(this).css('left');
+                var originalTop = $(this).css('top');
+              ;
+              
+                    if(ui.draggable.is('[data-timeline="' + droppableNumber + '"]')) {
+                        alert('the thing is dropped!');
+
+                        ui.draggable.addClass('dragged');
+                        $(this).addClass('dropped');
+
+
+                        ui.draggable.position({
+                            my: "center",
+                            at: "center",
+                            of: $(this),
+                            using: function(pos) {
+                                $(this).animate(pos, 200, "linear");
+                            }
+                        });
+
+                        ui.draggable.draggable('option', 'disabled', true);
+
+                       
+
+
+                    } else {
+                        ui.draggable.draggable('option', 'revert', 'valid');
+                    }
+            
+            }
+
+        });
+
+    }
+
     var bindEvents = function() {
-    	$(document).on('click tap', '.start-timeline-btn', displayDetailScreen);
+        $(document).on('click tap', '.start-timeline-btn', displayDetailScreen);
     }
 
     
