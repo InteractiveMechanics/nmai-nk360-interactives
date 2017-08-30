39d52f3594bfe9d17959dbcb1e2a2f132519bfbf
Christina Deemer <cahdeemer@gmail.com>
2017-08-30 16:57:13 -0400

    detail and explore screens

--- 2-timeline/js/controllers/detail.js
+++ 2-timeline/js/controllers/detail.js
@@ -12,28 +12,56 @@ Detail = (function() {
         buildGame();
     }
 
+    var countDroppedEls = function() {
+        var numberDropped = $('.dropped').length;
+        console.log(numberDropped);
+        if (numberDropped == 5) {
+            setTimeout(function() { displayExploreScreen(); }, 2000);
+        }
+    }
+
+    var displayExploreScreen = function() {
+        $('#explore').removeClass('animated fadeIn hidden');
+        $('#detail').addClass('fadeOut hidden');
+    }
+
+
+
+    var displayModal = function() {
+         $('#correct-answer').modal('show');
+    }
+
+    
+
 
     var buildGame = function() {
+        $('[data-toggle=tooltip]').tooltip('hide');
         $('.draggable-widget').draggable({
+            tolerance: 'touch',
             snap: '.droppable-widget',
-            //revert: 'invalid',
+            revert: 'invalid',
             snapMode: 'interior',
-            snapTolerance: 20,
-            cursorAt: {top: 10, left: 100}
+            snapTolerance: 10,
+            //cursorAt: {top: 10, left: 10}
         });
 
         $('.droppable-widget').droppable({
             drop: function( event, ui ) {
+                
+
                 var droppableNumber = $(this).data('timeline');
                 var originalLeft = $(this).css('left');
                 var originalTop = $(this).css('top');
-              ;
+              
               
                     if(ui.draggable.is('[data-timeline="' + droppableNumber + '"]')) {
+                        ui.draggable.addClass('dragged');
+
                         alert('the thing is dropped!');
 
                         ui.draggable.addClass('dragged');
                         $(this).addClass('dropped');
+                        displayModal();
 
 
                         ui.draggable.position({
@@ -46,12 +74,17 @@ Detail = (function() {
                         });
 
                         ui.draggable.draggable('option', 'disabled', true);
-
+                       
                        
 
 
                     } else {
                         ui.draggable.draggable('option', 'revert', 'valid');
+                        setTimeout(function() {   ui.draggable.tooltip('show'); }, 1000);
+
+                        setTimeout(function() {   ui.draggable.tooltip('hide'); }, 3000);
+                       
+                       
                     }
             
             }
@@ -62,6 +95,7 @@ Detail = (function() {
 
     var bindEvents = function() {
         $(document).on('click tap', '.start-timeline-btn', displayDetailScreen);
+        $(document).on('hidden.bs.modal', countDroppedEls);
     }
 
     
@@ -69,9 +103,24 @@ Detail = (function() {
 
 	return {
 
-		init: init
+		init: init,
+        displayExploreScreen: displayExploreScreen,
+        displayModal: displayModal
 	
 	}
 
 
 })();
+
+
+/*
+var template = $.templates("#theTmpl");
+
+var htmlOutput = template.render(data);
+
+$("#result").html(htmlOutput);
+
+$("#result").html($.templates("#theTmpl").render(data));
+*/
+
+
