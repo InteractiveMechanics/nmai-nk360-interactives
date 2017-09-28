402a1abae2111f7f462f3bb1e2290b6561ed66e0
Michael Tedeschi <mike.tedeschi@gmail.com>
2017-09-28 13:40:30 -0400

    Add uglify to Gruntfile

--- 5-news-article-creator/Gruntfile.js
+++ 5-news-article-creator/Gruntfile.js
@@ -66,6 +66,18 @@ module.exports = function(grunt) {
                 src: '../shared',
                 dest: 'dist/' + grunt.option('dir') + '/shared'
             }
+        },
+        uglify: {
+            options: {
+			    mangle: false
+		    },
+            dist: {
+                files: [{
+                    cwd: 'js',
+                    src: '**/*.js',
+                    dest: 'dist/' + grunt.option('dir') + '/js'
+                }]
+            }
         }
     });
     
@@ -78,7 +90,8 @@ module.exports = function(grunt) {
     grunt.loadTasks('../node_modules/grunt-contrib-copy/tasks');
     grunt.loadTasks('../node_modules/grunt-contrib-symlink/tasks');
     grunt.loadTasks('../node_modules/grunt-processhtml/tasks');
+    grunt.loadTasks('../node_modules/grunt-contrib-uglify/tasks');
     
     grunt.registerTask('dev', ['connect', 'watch']);
-    grunt.registerTask('dist', ['sass', 'copy', 'processhtml', 'symlink']);
+    grunt.registerTask('dist', ['sass', 'copy', 'processhtml', 'uglify', 'symlink']);
 };
