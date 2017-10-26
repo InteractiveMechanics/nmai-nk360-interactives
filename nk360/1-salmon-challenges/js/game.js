53202b831e8876d5bfb23fa34161c153833c098f
Jeff Majek <jmaj.email@gmail.com>
2017-10-26 13:38:06 -0400

    Come man alerts...

--- nk360/1-salmon-challenges/js/game.js
+++ nk360/1-salmon-challenges/js/game.js
@@ -400,9 +400,9 @@ Game = (function() {
       var encounters = gameData.forced;
       for (var i = 0; i < encounters.length; i++) {
         if((encounters[i].trigger_location - 200) == current_position) {
-          //settings.pause = false;
-          //createForcedEncounterSlider(encounters[i]);
-          alert(encounters[i].cards[0].title)
+          settings.pause = false;
+          createForcedEncounterSlider(encounters[i]);
+          //alert(encounters[i].cards[0].title)
         }
       };
     };
