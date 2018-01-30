94dfc3f4810b307f0aaaf563766aa9786bde4159
Michael Tedeschi <mike.tedeschi@gmail.com>
2018-01-30 14:06:15 -0500

    Incorporate android, landscape phone improvements

--- nk360/3-resource-annotator/js/app.js
+++ nk360/3-resource-annotator/js/app.js
@@ -5,6 +5,14 @@
 
 var data = function() {};
 $(function(){
+    if(/Android/.test(navigator.appVersion)) {
+        $('body').addClass('browser-android');
+        window.addEventListener("resize", function() {
+            if(document.activeElement.tagName=="INPUT" || document.activeElement.tagName=="TEXTAREA") {
+                document.activeElement.scrollIntoView();
+            }
+        });
+    }
     $.get(jsondata, function(response) {
         data = response;
 
